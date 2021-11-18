ab5f9a2e41cc40a9af48191ecaf40e7851cf6c90
krigora <ihor.kryvynskyy@icloud.com>
2021-11-17 20:33:01 -0600

    creating new classes

---
+++ src/day59_exeptions/CauseException.java
@@ -0,0 +1,9 @@
+package day59_exeptions;
+
+public class CauseException {
+    public static void main(String[] args) {
+
+     Throw new StringIndexOutOfBoundsException()
+
+    }
+}
---
+++ src/day59_exeptions/Person.java
@@ -0,0 +1,8 @@
+package day59_exeptions;
+
+public class Person {
+    private  String name;
+    private  int age;
+
+
+}
